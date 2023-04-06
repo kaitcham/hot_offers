973397144f7c438581cdfdaf95a087657d1c0360
Munezero Christian <chamsf5@aol.com>
2023-04-06 08:22:14 +0200

    Create a utils file to mock up regions links

---
+++ src/utils/Regions.jsx
@@ -0,0 +1,24 @@
+const regions = [
+  {
+    id: 1,
+    regionName: 'huye',
+  },
+  {
+    id: 2,
+    regionName: 'muhanga',
+  },
+  {
+    id: 3,
+    regionName: 'kigali',
+  },
+  {
+    id: 4,
+    regionName: 'musanze',
+  },
+  {
+    id: 5,
+    regionName: 'rubavu',
+  },
+];
+
+export default regions;
