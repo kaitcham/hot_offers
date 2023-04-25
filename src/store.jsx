b507a3f8feb5b14d94780bc1c6da7fe4d025d143
Munezero Christian <chamsf5@aol.com>
2023-04-25 19:38:58 +0200

    Create application store

---
+++ src/store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import appReducer from './features/appSlice';
+
+const store = configureStore({
+  reducer: {
+    app: appReducer,
+  },
+});
+export default store;
