b2f1d35d1fe693a7ac135af8a52606ee3b6b3eee
Munezero Christian <chamsf5@aol.com>
2023-04-25 19:52:35 +0200

    Sign Up and Sign In Page

--- src/pages/authPages/LoginPage.jsx
+++ src/pages/authPages/LoginPage.jsx
@@ -1,63 +1,131 @@
-import { Link } from "react-router-dom";
+import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { setIsMember, setPreviousPath } from '../../features/appSlice';
 
 const LoginPage = () => {
+  const dispatch = useDispatch();
+  const { state } = useLocation();
+  const { isMember } = useSelector((state) => state.app);
+
+  useEffect(() => {
+    dispatch(setPreviousPath(state?.previousPath));
+  }, [state]);
+
   return (
-    <div>
-      <div className="login pt-20 border pl-[450px] pt-[80px] pb-[100px]">
-        <div className="w-full max-w-sm px-4 py-4 bg-white border border-gray-400 item-center rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <form className="space-y-6" action="#">
-            <h5 className="text-xl pt-5 text-center font-medium text-gray-900 dark:text-white">
-              Login
-            </h5>
+    <div className="bg-gray-100 h-screen flex justify-center items-center">
+      <div className="bg-white border border-transparent shadow-xl rounded-md px-5 py-4 w-full max-w-md relative">
+        <div className="absolute top-0 right-0 p-2 bg-red-900">
+          <Link
+            to={state?.previousPath}
+            onClick={() => dispatch(setIsMember(true))}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
+        </div>
+        <div className="w-fit mx-auto">
+          <h3 className="text-2xl pb-1">
+            {isMember ? 'Login' : 'Create Account'}
+          </h3>
+          <div className="bg-red-800 h-0.5 w-4/5 mx-auto" />
+        </div>
+        <form className="pt-8">
+          <div className="flex flex-col">
+            {!isMember && (
+              <>
+                <div>
+                  <input
+                    type="text"
+                    name="firstName"
+                    placeholder="First Name"
+                    className="w-full p-2 mb-5 border border-gray-500 rounded-[4px] focus:outline-none"
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                    className="w-full p-2 mb-5 border border-gray-500 rounded-[4px] focus:outline-none"
+                  />
+                </div>
+                <div className="order-last">
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirm Password"
+                    className="w-full p-2 mb-2 border border-gray-500 rounded-[4px] focus:outline-none"
+                  />
+                </div>
+              </>
+            )}
             <div>
               <input
                 type="email"
                 name="email"
-                id="email"
-                className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="Enter Your Email"
-                required
+                placeholder="Email"
+                className="w-full p-2 mb-5 border border-gray-500 rounded-[4px] focus:outline-none"
               />
             </div>
             <div>
               <input
                 type="password"
                 name="password"
-                id="password"
-                placeholder="Enter your password"
-                className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                required
+                placeholder="Password"
+                className="w-full p-2 mb-5 border border-gray-500 rounded-[4px] focus:outline-none"
               />
             </div>
-            <div className="flex items-start">
-              <div className="flex items-start"></div>
+          </div>
+          {isMember ? (
+            <div className="flex justify-between items-center">
+              <span className="flex items-center">
+                <input type="checkbox" name="remember" id="remember" />
+                <label
+                  htmlFor="remember"
+                  className="text-sm ml-1 cursor-pointer"
+                >
+                  Remember me
+                </label>
+              </span>
+              <Link to="/forgotPassword" className="text-sm text-red-800">
+                Forgot Password?
+              </Link>
             </div>
+          ) : (
+            <p className="text-xs text-gray-400">
+              Password must be atleast 8 characters
+            </p>
+          )}
+          <button
+            type="submit"
+            className="w-full bg-red-800 hover:bg-red-900 text-white py-2 rounded-[4px] mt-6"
+          >
+            {isMember ? 'Sign In' : 'Sign Up'}
+          </button>
+          <div className="flex justify-center items-center mt-6">
+            <span className="text-sm">
+              {isMember ? "Don't have an account?" : 'Already have an account?'}
+            </span>
             <button
-              type="submit"
-              className="w-full text-white bg-red-700 hover:red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+              className="text-sm text-red-800 ml-3"
+              onClick={() => dispatch(setIsMember())}
             >
-              Sign in
+              {isMember ? 'Sign Up' : 'Sign In'}
             </button>
-            <div className="text-sm font-medium text-gray-500 dark:text-gray-300 text-center">
-              you dont have an account?{" "}
-              <Link
-                to="/signup"
-                className="text-red-700 hover:underline dark:text-blue-500"
-              >
-                Sign up
-              </Link>
-            </div>
-            <div className="text-sm font-medium text-gray-500 dark:text-gray-300 text-center">
-              Forgot Password?{" "}
-              <Link
-                to="/resetPassword"
-                className="text-red-700 hover:underline dark:text-blue-500"
-              >
-                Reset
-              </Link>
-            </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   );
