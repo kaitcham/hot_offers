4c7ea5578d7b1055bb273e42a3273b22cc8ffcd0
Munezero Christian <chamsf5@aol.com>
2023-04-25 19:47:04 +0200

    Import and export forget password page

--- src/pages/_index.jsx
+++ src/pages/_index.jsx
@@ -1,6 +1,13 @@
-import LandingPage from "./LandingPage";
-import RegionProducts from "./RegionProducts";
-import LoginPage from "./authPages/LoginPage";
-import ResetPasswordPage from "./authPages/ResetPasswordPage";
+import LandingPage from './LandingPage';
+import RegionProducts from './RegionProducts';
+import LoginPage from './authPages/LoginPage';
+import ResetPasswordPage from './authPages/ResetPasswordPage';
+import ForgotPasswordPage from './authPages/ForgotPasswordPage';
 
-export { LandingPage, RegionProducts, LoginPage, ResetPasswordPage };
+export {
+  LandingPage,
+  RegionProducts,
+  LoginPage,
+  ResetPasswordPage,
+  ForgotPasswordPage,
+};
