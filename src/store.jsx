6f617e549f9d0fe4d33b2d9012a38d4abe74dc6f
Munezero Christian <chamsf5@aol.com>
2023-04-28 09:09:28 +0200

    Add authSlice to redux store

--- src/store.jsx
+++ src/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appReducer from './features/appSlice';
+import authReducer from './features/auth/authSlice';
 
 const store = configureStore({
   reducer: {
     app: appReducer,
+    auth: authReducer,
   },
 });
 export default store;
