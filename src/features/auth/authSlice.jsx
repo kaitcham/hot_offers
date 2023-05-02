d8eaba6cf1074877cee65467046190a1768839e8
Munezero Christian <chamsf5@aol.com>
2023-05-02 15:47:45 +0200

    Login member action

--- src/features/auth/authSlice.jsx
+++ src/features/auth/authSlice.jsx
@@ -1,6 +1,6 @@
 import { toast } from 'react-toastify';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { createMemberThunk } from './authThunk';
+import { createMemberThunk, loginMemberThunk } from './authThunk';
 
 const initialState = {
   user: {},
@@ -13,6 +13,11 @@ export const createMember = createAsyncThunk(
   createMemberThunk
 );
 
+export const loginMember = createAsyncThunk(
+  'auth/loginMember',
+  loginMemberThunk
+);
+
 const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -23,13 +28,27 @@ const authSlice = createSlice({
     });
     builder.addCase(createMember.fulfilled, (state, action) => {
       state.status = 'success';
-      state.user = action.payload;
       toast.success('Account created successfully');
     });
     builder.addCase(createMember.rejected, (state, action) => {
       state.status = 'failed';
       state.error = action.payload;
       toast.error('User Registration failed');
+    });
+    builder.addCase(loginMember.pending, (state) => {
+      state.status = 'loading';
+    });
+    builder.addCase(loginMember.fulfilled, (state, action) => {
+      state.status = 'success';
+      state.user = action.payload;
+      toast.success('Login successful');
+    });
+    builder.addCase(loginMember.rejected, (state, action) => {
+      state.status = 'failed';
+      const { non_field_errors } = action.payload;
+      non_field_errors
+        ? toast.error(non_field_errors[0])
+        : toast.error(action.payload.message);
     });
   },
 });
