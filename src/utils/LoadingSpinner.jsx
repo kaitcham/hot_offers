1006a5ce46bd7d0adbe0b158d6365473e639b07c
Munezero Christian <chamsf5@aol.com>
2023-05-03 14:12:08 +0200

    Modify loading spinner styles

--- src/utils/LoadingSpinner.jsx
+++ src/utils/LoadingSpinner.jsx
@@ -4,13 +4,13 @@ const LoadingSpinner = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <motion.div
-        className="flex items-center justify-center w-16 h-16 rounded-full bg-spinner"
+        className="flex items-center justify-center w-20 h-20 rounded-full bg-spinner"
         animate={{ rotate: 360 }}
         transition={{ duration: 1, repeat: Infinity, ease: 'linear' }}
       >
-        <div className="bg-white rounded-full h-12 w-12"></div>
+        <div className="bg-white rounded-full h-14 w-14"></div>
       </motion.div>
-      <p className="text-red-700 ml-1">Loading...</p>
+      <p className="text-red-700 text-sm mt-2">Loading...</p>
     </div>
   );
 };
