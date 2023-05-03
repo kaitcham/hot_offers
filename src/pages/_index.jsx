0ee86ba4b035cc060bab8557a8e9a37bf84afe6d
Munezero Christian <chamsf5@aol.com>
2023-05-03 14:17:57 +0200

    Import and export protected page component

--- src/pages/_index.jsx
+++ src/pages/_index.jsx
@@ -1,13 +1,15 @@
 import LandingPage from './LandingPage';
+import ProtectedPage from './ProtectedPage';
 import RegionProducts from './RegionProducts';
 import LoginPage from './authPages/LoginPage';
 import ResetPasswordPage from './authPages/ResetPasswordPage';
 import ForgotPasswordPage from './authPages/ForgotPasswordPage';
 
 export {
-  LandingPage,
-  RegionProducts,
   LoginPage,
+  LandingPage,
+  ProtectedPage,
+  RegionProducts,
   ResetPasswordPage,
   ForgotPasswordPage,
 };
