726fbf261a9ede51a2057d42394acb547c50448d
Munezero Christian <chamsf5@aol.com>
2023-05-04 12:31:24 +0200

    Add axios interceptors to add token in the request

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,21 @@
 import axios from 'axios';
+import { getUserFromLocalStorage } from './localStorage';
 
 const customBaseUrl = axios.create({
   baseURL: 'https://hotoffers-backend-production.up.railway.app/api',
 });
+
+customBaseUrl.interceptors.request.use(
+  (config) => {
+    const user = getUserFromLocalStorage();
+    if (user) {
+      config.headers['Authorization'] = `Token ${user.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default customBaseUrl;
